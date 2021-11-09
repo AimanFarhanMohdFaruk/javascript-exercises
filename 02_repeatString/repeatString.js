6979a677a24a4242895c340d2dd01e1b66dc861d
Aiman Farhan <aimanfaruk98@gmail.com>
2021-11-10 00:06:01 +0800

    passed all test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    let multiStr = ""
 
+    if(num >= 0){
+        for (let i = 0; i < num; i++){
+            multiStr += str
+        }
+    
+        return multiStr
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
