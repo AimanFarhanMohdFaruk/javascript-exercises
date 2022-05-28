171b0c0a6f364b96ce8292b1e91a2394081b7764
Aiman Farhan <aiman.faruk@surialabs.com>
2022-05-28 16:31:47 +0800

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,16 +6,22 @@ const subtract = function() {
 	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length <= 0) return 0;
+  return array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+  )
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length <= 0) return 0;
+  return array.reduce(
+    (previousValue, currentValue) => previousValue * currentValue
+  )
 };
 
-const power = function() {
-	
+const power = function(number,power) {
+	return Math.pow(number,power)
 };
 
 const factorial = function() {
